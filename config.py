b4ca5a00f95acf3c4fefdb9fe2e35677b92c4286
Ali Sezer <mustafaali.sezer@databricks.com>
2023-08-16 21:16:07 +0100

    adds sampling to config

--- config.py
+++ config.py
@@ -1,4 +1,9 @@
 """Global Variables"""
 
-CATALOG_NAME = "rs_sol_acc"
-SCHEMA_NAME = "review_summarisation"
+# Data Globals (All Notebooks)
+CATALOG_NAME = "rs_sol_acc"  # Unity Catalog Name
+SCHEMA_NAME = "review_summarisation"  # Schema Name
+
+# Sampling Parameters (0.2-explore-prep)
+TOP_BOOK_COUNT = 1000  # How many popular books to take ?
+TOP_BOOK_SAMPLING_FRACTION = 0.01  # Sample further ? (None = no sampling)
