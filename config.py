c7a4cbff7d47af459e1c6f8f4f35f9aada270ac0
dbbnicole <dbbnicole>
2023-08-17 22:34:24 +0000

    Add repo links; remove unnecessary sample files

--- config.py
+++ config.py
@@ -4,7 +4,7 @@
 ################################################################
 # Data Globals (All Notebooks)
 
-CATALOG_NAME = "rcg_rs_sol_acc"  # Unity Catalog Name
+CATALOG_NAME = "solacc_cicd"  # Unity Catalog Name
 SCHEMA_NAME = "review_summarisation"  # Schema Name
 
 # UC Options (All Notebooks)
